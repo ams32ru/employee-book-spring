5d80e937884cd0a479bc8519bac1d55b2fe83612
ams32ru <ams32ru@mail.ru>
2022-11-12 22:25:21 +0300

    Initial commit

---
+++ src/main/java/com/skypro/employee/model/Employee.java
@@ -0,0 +1,50 @@
+package com.skypro.employee.model;
+
+public class Employee {
+    private static int counter;
+    private final int id;
+    private final String firstName;
+    private final String lastName;
+    private final int departament;
+    private final int salary;
+
+    public Employee(String firstName, String lastName, int departament, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.departament = departament;
+        this.salary = salary;
+
+        this.id =counter++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getDepartament() {
+        return departament;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", departament=" + departament +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ src/main/java/com/skypro/employee/service/EmployeeService.java
@@ -0,0 +1,72 @@
+package com.skypro.employee.service;
+
+import com.skypro.employee.model.Employee;
+import com.skypro.employee.record.EmployeeRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class EmployeeService {
+    private final Map<Integer, Employee> employees = new HashMap<>();
+
+
+    public Collection<Employee> getAllEmployees() {
+        return this.employees.values();
+    }
+
+    public Employee addEmployee(EmployeeRequest employeeRequest) {
+        if (employeeRequest.getFirstName() == null || employeeRequest.getLastName() == null) {
+            throw new IllegalArgumentException(" Не введено имя или фамилия");
+        }
+        Employee employee = new Employee(employeeRequest.getFirstName(),
+                employeeRequest.getLastName(),
+                employeeRequest.getDepartament(),
+                employeeRequest.getSalary());
+        this.employees.put(employee.getId(),employee);
+        return employee;
+    }
+
+    public int getSalarySum() {
+        return employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .sum();
+    }
+
+    public Employee getSalaryMin() {
+        int idMin = 0;
+        int max = Integer.MAX_VALUE;
+        for (Employee value : employees.values()) {
+            if (value.getSalary() < max) {
+                max= value.getSalary();
+               idMin = value.getId();
+            }
+        }
+        return employees.get(idMin);
+    }
+
+    public Employee getSalaryMax() {
+        int idMax = 0;
+        int min = Integer.MIN_VALUE;
+        for (Employee value : employees.values()) {
+            if (value.getSalary() > min) {
+                min=value.getSalary();
+                idMax = value.getId();
+            }
+        }
+        return employees.get(idMax);
+    }
+
+
+    public List getHighSalary() {
+        List <Employee> highEmployee = new ArrayList<>();
+       int average = getSalarySum()/employees.size();
+        for (Employee value : employees.values()) {
+            if (value.getSalary() > average) {
+                highEmployee.add(value);
+            }
+        }
+        return highEmployee;
+    }
+
+}
